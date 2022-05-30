44485c8a3a31e05844363720d901b3eeeedf261f
Luis_Gabriel <luis.g.s.pallazzi@gmail.com>
2022-05-30 17:17:02 -0300

    :bug: Correção Bug Menu

--- script.js
+++ script.js
@@ -4,4 +4,10 @@ hamburguer.addEventListener("click", function(){
     document.querySelector(".container").classList.toggle("show-menu");
 });
 
-<script src="js/preloader.js"></script>
+
+document.querySelector("#qtde").addEventListener("click", atualizarPreco)
+
+function atualizarPreco(){
+    const qtde = document.querySelector("#qtde").value  
+    console.log(qtde)
+}
